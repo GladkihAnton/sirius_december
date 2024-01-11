2e41e802363dd940d9c05a5cd762d726b2ce0153
Пушкарева Алина Александровна <linapush@MacBook-Air-Puskareva.local>
2024-01-11 14:16:54 +0300

    доработки с ASGI

--- webapp/__main__.py
+++ webapp/__main__.py
@@ -1,4 +1,10 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('webapp.main:create_app', host='0.0.0.0', port=8000, factory=True, reload=True)
+    uvicorn.run(
+        'webapp.main:create_app',
+        host='0.0.0.0',
+        port=8000,
+        factory=True,
+        reload=True,
+    )
