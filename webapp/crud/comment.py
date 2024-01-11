fac674a753bbd6077756033105a12a8c7f0057f1
Пушкарева Алина Александровна <linapush@MacBook-Air-Puskareva.local>
2024-01-11 14:06:33 +0300

    доработка crud

--- webapp/crud/comment.py
+++ webapp/crud/comment.py
@@ -1,27 +1,35 @@
-# crud for comment
+from typing import List, Tuple
 
-from typing import List
-
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from webapp.models.sirius.comment import Comment
 
 
-async def create_comment(session: AsyncSession, content: str, author_id: int, post_id: int) -> Comment:
-    new_comment = Comment(content=content, author_id=author_id, post_id=post_id)
+async def create_comment(
+    session: AsyncSession, content: str, author_id: int, post_id: int
+) -> Comment:
+    new_comment = Comment(
+        content=content, author_id=author_id, post_id=post_id
+    )
     session.add(new_comment)
     await session.commit()
     await session.refresh(new_comment)
     return new_comment
 
 
-async def get_comment_by_id(session: AsyncSession, comment_id: int) -> Comment | None:
-    result = await session.scalars(select(Comment).where(Comment.id == comment_id))
+async def get_comment_by_id(
+    session: AsyncSession, comment_id: int
+) -> Comment | None:
+    result = await session.scalars(
+        select(Comment).where(Comment.id == comment_id)
+    )
     return result.one_or_none()
 
 
-async def update_comment(session: AsyncSession, comment_id: int, new_content: str) -> Comment | None:
+async def update_comment(
+    session: AsyncSession, comment_id: int, new_content: str
+) -> Comment | None:
     comment = await get_comment_by_id(session, comment_id)
     if comment:
         comment.content = new_content
@@ -40,6 +48,21 @@ async def delete_comment(session: AsyncSession, comment_id: int) -> bool:
     return False
 
 
-async def get_comments_by_post(session: AsyncSession, post_id: int) -> List[Comment]:
-    result = await session.scalars(select(Comment).where(Comment.post_id == post_id))
-    return list(result.all())
+async def get_comments_by_post(
+    session: AsyncSession, post_id: int, page: int, per_page: int
+) -> Tuple[List[Comment], int]:
+    offset = (page - 1) * per_page
+    comments_query = (
+        select(Comment)
+        .where(Comment.post_id == post_id)
+        .limit(per_page)
+        .offset(offset)
+    )
+    total_query = select(func.count(Comment.id)).where(
+        Comment.post_id == post_id
+    )
+    comments_result = await session.scalars(comments_query)
+    total_result = await session.scalar(total_query)
+    comments = list(comments_result.all())
+    total_comments = total_result
+    return comments, total_comments
--- webapp/crud/post.py
+++ webapp/crud/post.py
@@ -1,19 +1,24 @@
-# crud for post
+from typing import List, Tuple
 
-from typing import List
-
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from webapp.models.sirius.post import Post
 
 
-async def get_all_posts(session: AsyncSession) -> List[Post]:
-    result = await session.scalars(select(Post))
-    return list(result.all())
+async def get_all_posts(
+    session: AsyncSession, page: int, per_page: int
+) -> Tuple[List[Post], int]:
+    offset = (page - 1) * per_page
+    result = await session.scalars(select(Post).limit(per_page).offset(offset))
+    posts = list(result.all())
+    total_posts = await session.scalar(select(func.count(Post.id)))
+    return posts, total_posts
 
 
-async def create_post(session: AsyncSession, content: str, author_id: int) -> Post:
+async def create_post(
+    session: AsyncSession, content: str, author_id: int
+) -> Post:
     new_post = Post(content=content, author_id=author_id)
     session.add(new_post)
     await session.commit()
@@ -26,7 +31,9 @@ async def get_post_by_id(session: AsyncSession, post_id: int) -> Post | None:
     return result.one_or_none()
 
 
-async def update_post(session: AsyncSession, post_id: int, new_content: str) -> Post | None:
+async def update_post(
+    session: AsyncSession, post_id: int, new_content: str
+) -> Post | None:
     post = await get_post_by_id(session, post_id)
     if post:
         post.content = new_content
@@ -44,6 +51,18 @@ async def delete_post(session: AsyncSession, post_id: int) -> bool:
     return False
 
 
-async def get_posts_by_user(session: AsyncSession, user_id: int) -> List[Post]:
-    result = await session.scalars(select(Post).where(Post.author_id == user_id))
-    return list(result.all())
+async def get_posts_by_user(
+    session: AsyncSession, user_id: int, page: int, per_page: int
+) -> Tuple[List[Post], int]:
+    offset = (page - 1) * per_page
+    result = await session.scalars(
+        select(Post)
+        .where(Post.author_id == user_id)
+        .limit(per_page)
+        .offset(offset)
+    )
+    posts = list(result.all())
+    total_posts = await session.scalar(
+        select(func.count()).where(Post.author_id == user_id).select_from(Post)
+    )
+    return posts, total_posts
