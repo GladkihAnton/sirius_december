42221ee7d2c77a57d8bf46135ed0cc93d4690b90
annashutova <annashut@inbox.ru>
2024-01-12 01:32:28 +0000

    journal pydantic models

--- webapp/schema/journal.py
+++ webapp/schema/journal.py
@@ -1,14 +1,14 @@
 from datetime import date, datetime
-from typing import Optional
+from typing import List, Optional
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, ConfigDict, field_validator
+
+from webapp.schema.subject import SubjectBase
 
 
-class JournalInfo(BaseModel):
+class JournalBase(BaseModel):
     grade: Optional[int]
     class_date: date
-    subject_id: int
-    student_id: int
 
     @field_validator('class_date')
     @classmethod
@@ -18,3 +18,67 @@ class JournalInfo(BaseModel):
         elif isinstance(value, str):
             return datetime.strptime(value, '%Y-%m-%d').date()
         raise ValueError('Invalid type. Must be string or date.')
+
+
+class JournalInfo(JournalBase):
+    subject_id: int
+    student_id: int
+
+
+class JournalData(JournalBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+
+
+class SubjectWithJournal(SubjectBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    records: List[JournalData]
+
+
+class StudentsJournal(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    first_name: str
+    last_name: str
+    surname: Optional[str] = None
+    birthdate: date
+    subjects: List[SubjectWithJournal]
+
+    @field_validator('birthdate')
+    @classmethod
+    def parse_birthdate(cls, value: str | date) -> date:
+        if isinstance(value, date):
+            return value
+        elif isinstance(value, str):
+            return datetime.strptime(value, '%Y-%m-%d').date()
+        raise ValueError('Invalid type. Must be string or date.')
+
+
+"""
+[
+    {
+        id,
+        first_name,
+        last_name,
+        surname,
+        birthdate,
+        subjects: [
+            {
+                id,
+                title,
+                records: [
+                    {
+                        id,
+                        grade,
+                        class_date
+                    }
+                ]
+            }
+        ]
+    }
+]
+"""
